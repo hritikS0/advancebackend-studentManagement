a0989d3b01610da0c063061d983d676ccda0d84c
hritikS0 <sharmahritik8077@gmail.com>
2026-07-02 23:27:11 +0530

    changes in user repository

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -1,14 +1,22 @@
 import prisma from "../../database/prisma.js";
-
+import type { UserQuery, UpdateUserDto } from "./user.types.js";
 export class UserRespository {
   async findById(id: string) {
     return prisma.user.findUnique({
       where: {
         id,
       },
+      select: {
+        id: true,
+        name: true,
+        emai: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
     });
   }
-  async findAll(query) {
+  async findAll(query: UserQuery) {
     const {
       page,
       limit,
@@ -16,18 +24,12 @@ export class UserRespository {
       sortBy = "createdAt",
       sortOrder = "desc",
     } = query;
-
+    const skip = (page - 1) * limit;
     const where = search
       ? {
           OR: [
             {
-              firstName: {
-                contains: search,
-                mode: "insensitive" as const,
-              },
-            },
-            {
-              lastName: {
+              name: {
                 contains: search,
                 mode: "insensitive" as const,
               },
@@ -41,6 +43,37 @@ export class UserRespository {
           ],
         }
       : {};
-    // todo
+    const [user, total] = await Promise.all([
+      prisma.user.findMany({
+        where,
+        take: limit,
+        skip,
+        orderBy: {
+          [sortBy]: sortOrder,
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          role: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      }),
+      prisma.user.count({
+        where,
+      }),
+    ]);
+    return {
+      meta: {
+        page,
+        limit,
+        total,
+        totalPages: Math.ceil(total / limit),
+      },
+      data: user,
+    };
   }
+  async update(id: string, data: UpdateUserDto) {}
+  async delete(id: string) {}
 }
--- src/modules/user/user.types.ts
+++ src/modules/user/user.types.ts
@@ -1,3 +1,11 @@
 export interface UpdateUserDto {
   name?: string;
 }
+
+export interface UserQuery {
+  page: number;
+  limit: number;
+  search?: string;
+  sortBy?: string;
+  sortOrder?: "asc" | "desc";
+}
